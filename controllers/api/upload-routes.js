2147c19cbb97edc9a75ae9136a926b5e4db74f6f
Andy Kleindienst <andy.kleindienst@gmail.com>
2023-03-16 21:46:36 -0400

    working with cloudinary

--- controllers/api/upload-routes.js
+++ controllers/api/upload-routes.js
@@ -1,17 +1,39 @@
 const router = require("express").Router();
+const multer = require("multer");
+const storage = multer.memoryStorage();
+const multerUploads = multer({ storage }).single("image");
+const cloudinary = require("cloudinary").v2;
 
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+});
 
-//image upload post
-router.post('/upload', (req, res) => {
-  let sampleFile;
-  let uploadPath;
-  if(!req.files || Object.keys(req.files).length === 0){
-    return res.status(400).send('No files were uploaded.');
+//image upload get
+router.get("/", async (req, res) => {
+  try {
+    const response = await cloudinary.uploader.upload(
+      "https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+      { public_id: "olympic_flag" }
+    );
+    console.log(response.json);
+    console.log(response.secure_url.json);
+
+    // Generate
+    const url = cloudinary.url("olympic_flag", {
+      width: 100,
+      height: 150,
+      Crop: "fill",
+    });
+
+    // The output url
+    console.log(url);
+    // https://res.cloudinary.com/<cloud_name>/image/upload/h_150,w_100/olympic_flag
+  } catch (err) {
+    console.log(err);
   }
-  //name of the input is samplefile
-  sampleFile = req.files.sampleFile;
-  console.log(sampleFile);
-
-})
+});
 
 module.exports = router;
+module.exports = multerUploads;
